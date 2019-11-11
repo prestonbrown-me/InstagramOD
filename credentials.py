dcbfff56114be5dc83600ba26cd10257a0b0f91a
Preston Brown <5788534+phbprogramming@users.noreply.github.com>
2019-11-11 14:09:53 -0600

    update credentials to also hold botnet logins

--- credentials.py
+++ credentials.py
@@ -1,3 +1,9 @@
 mongo_url = 'mongodb+srv://USER:PASSWORD@CLUSTERURL'
 instagram_username = 'USERNAME'
 instagram_password = 'PASSWORD'
+
+# FOR SCRAPER BOTS, ADD THEM INTO THE LOGINS DICTIONARY LIKE SO
+# YOU CAN ADD AS MANY AS YOU LIKE
+# botlogins['username'] = 'Password'
+
+botlogins = {}
