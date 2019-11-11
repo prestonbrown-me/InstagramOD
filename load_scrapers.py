71435fbef86056331bbc2f3b97d9701145479136
phbprogramming <phbprogramming+git@gmail.com>
2019-11-11 14:15:22 -0600

    changed load_scrapers to reflect new credentials location of bots

--- load_scrapers.py
+++ load_scrapers.py
@@ -1,21 +1,16 @@
 from profile_scraper import ProfileScraper
 from OverdriveDB import Mongoloid
 from MainAutomation import MainAutomation
+import credentials
 
 m = Mongoloid()
-
-logins = {}
-
-# FOR SCRAPER BOTS, ADD THEM INTO THE LOGINS DICTIONARY LIKE SO
-# YOU CAN ADD AS MANY AS YOU LIKE
-# logins['username'] = 'Password'
 
 main_automation = MainAutomation()
 main_automation.start()
 
 threads = []
-for item in logins:
-    scraper = ProfileScraper(item, logins[item], m)
+for item in credentials.botlogins:
+    scraper = ProfileScraper(item, credentials.botlogins[item], m)
     threads.append(scraper)
     scraper.start()
 
