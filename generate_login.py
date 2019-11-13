21a20d42a69375bbc7937b525ef8b9b74d00a351
Preston Brown <phbprogramming@gmail.com>
2019-11-13 17:05:01 -0600

    Create loginsessions folder if it doesn't exist

--- generate_login.py
+++ generate_login.py
@@ -1,8 +1,13 @@
 import pickle
 import credentials
 from InstagramAPI import InstagramAPI
+import os
 
 def logIn():
+
+    if not os.path.exists('loginsessions'):
+        os.makedirs('loginsessions')
+
     # log in with instagram service
     api = InstagramAPI(credentials.instagram_username, credentials.instagram_password)
     api.login()
